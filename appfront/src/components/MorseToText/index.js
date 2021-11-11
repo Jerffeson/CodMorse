179bc3e11f21ee72bde481f21b478fb2c41e27cd
Jerffeson <jerffesongsantos@gmail.com>
2021-11-11 09:14:33 -0300

    Component React para capturar o código morse, comunicar com API e mostrar resultado

--- appfront/src/components/MorseToText/index.js
+++ appfront/src/components/MorseToText/index.js
@@ -1,13 +1,18 @@
 import React,  {useState, useEffect} from 'react';
 import { Formik, Form, Field, ErrorMessage } from 'formik';
 import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import axios from "axios";
 import './styles.css';
 
 const MorseToText = (props) => {
-    const [text, setText] = useState("");
+    const [text, setText] = useState(" ");
     return (
         <div>
+            <header>
+                <h1>CodMorse</h1>
+                <p>Converta seu código morse em texto</p>
+            </header>
             <Formik
                 enableReinitialize 
                 initialValues={{cod_morse:""}}
@@ -30,15 +35,24 @@ const MorseToText = (props) => {
                       });
                       
                     await api.get('/api/morse', {params: param})
-                    .then(function (response) {
-                        setText(text+response.data.data);                                                 
-                    });              
+                    .then(function (response) {  
+                        if(response.data.data){
+                            setText(text+response.data.data);                                                 
+                        }
+                        else{
+                            toast.warn("Digite um código morse válido.");
+                        }
+                    }).catch(
+                        function (response) {
+                            toast.error("Problema interno. Tente novamente!");
+                        }
+                    );              
                 }}
                 >
                 {({ isSubmitting }) => (
                     <Form className="form-padrao">
-                        <label htmlFor="cod_morse">Código Morse</label>
-                        <Field type="text" name="cod_morse" placeholder=". ou -"/>
+                        <label htmlFor="cod_morse">Código Morse:</label>
+                        <Field type="text" name="cod_morse" placeholder=". ou -" required maxLength='5'/>
                         <ErrorMessage name="cod_morse" component="div" className="error-message"/>
      
                         <div className="submit-box">                            
@@ -51,8 +65,11 @@ const MorseToText = (props) => {
                     </Form>
                 )}
                 </Formik>
-                <ToastContainer />
-                <h1>{text}</h1>
+                <ToastContainer position="bottom-right" />                
+                <div className="box-text">
+                    <h4>Resultado em texto:</h4>
+                    <h2>{text}</h2>
+                </div>
         </div>
     );
 }
